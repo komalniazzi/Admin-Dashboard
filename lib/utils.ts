a3e884b17dea25f8ffa6ea58014c5a402ff3a8c6
komalniazzi <119024937+komalniazzi@users.noreply.github.com>
2025-06-12 11:17:51 +0500

    Improved cn function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,8 @@
 // lib/utils.ts
-export function cn(...classes: (string | boolean | undefined | null)[]) {
-  return classes.filter(Boolean).join(' ')
+// utils/cn.ts
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: any[]) {
+  return twMerge(clsx(...inputs));
 }
